f6c001fdffce31842332f6372ebee5da77345774
Parth Pandey <parthpandey19@gmail.com>
2024-05-08 02:07:20 +0200

    new features

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -196,3 +196,28 @@ class TimeSeriesSplit(_BaseKFold):
                     indices[:train_end],
                     indices[test_start : test_start + test_size],
                 )
+
+
+def make_model(src_vocab, tgt_vocab, n=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    """
+    Helper: Construct a model from hyperparameters.
+    """
+    c = copy.deepcopy
+    attn = MultiHeadAttention(h, d_model)
+    ff = PointerwiseFeedforward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), n),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), n),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab)
+    )
+
+    # This was important from their code.
+    # Initialize parameters with Glorot / fan_avg.
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    return model
